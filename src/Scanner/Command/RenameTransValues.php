f3f4ce68b0909c1edf4240196735424b0165b77d
Roman Taranenko <roman@aruodas.lt>
2016-06-20 15:50:03 +0300

    Translation loading, exception handling

--- src/Scanner/Command/RenameTransValues.php
+++ src/Scanner/Command/RenameTransValues.php
@@ -3,11 +3,13 @@
 namespace Scanner\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\FormatterHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Scanner\Service;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class RenameTransValues extends Command
 {
@@ -26,14 +28,41 @@ class RenameTransValues extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
+
+
         /**
          * @var $unit \SplFileInfo
+         * @var $formatter FormatterHelper
          */
 
         $root = $input->getArgument('root');
+
         $scanner = new Service\RenameDirectoryIterator($root);
         $filter = new Service\RenameIteratorFilter($scanner);
         $iterator = new Service\RenameIterator($filter);
+
+        try {
+            $translationsPath = $root . 'app/autoplius/translation/messages.en.php';
+
+            if (!file_exists($translationsPath)) {
+                throw new \Exception;
+            }
+
+            $translations = require_once $translationsPath;
+            $keys = array_keys($translations);
+
+            if (empty($keys)) {
+                throw new \Exception;
+            }
+
+            $io->success(['Translations file found. Loaded ' .count($keys) . ' keys']);
+        } catch(\Exception $e) {
+            $io->error(['Translation file not found or is empty.']);
+            exit;
+        }
+
+        $io->note('Process started');
 
         foreach ($iterator as $path => $unit) {
             if (!$unit->isDir()) {
@@ -41,7 +70,16 @@ class RenameTransValues extends Command
                     continue;
                 }
 
-                $output->writeln('<info>' . $unit->getRealPath() . '</info>');
+                $output->writeln($unit->getRealPath());
+                if ($unit->isWritable()) {
+                    $file = $unit->openFile('a+');
+                    $content = $file->fread($file->getSize());
+                    if (strpos($content, '__(') !== false) {
+                        $output->writeln(strpos($content, '__('));
+                        break;
+                    }
+//                    $file->fwrite("appended this sample text");
+                }
             }
         }
     }
